ad83b3d0d761753f9ccc30d97118450549f13f36
Naoto Kamba <naoto.kamba@outlook.jp>
2024-05-22 01:01:57 +0900

    wip

--- lib/aws-stack.ts
+++ lib/aws-stack.ts
@@ -1,11 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
 export class AwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    // const vpc = new ec2.Vpc(this, "SampleVpca", {
+    //   vpcName: "sample-vpc-changed",
+    //   ipAddresses: ec2.IpAddresses.cidr("172.16.0.0/16"),
+    //   subnetConfiguration: [],
+    //   natGateways: 0
+    // });
     // The code that defines your stack goes here
 
     // example resource
