1721ba574444c862e8b1bb510748a9aaa4ab9240
Naoto Kamba <naoto.kamba@outlook.jp>
2024-05-22 20:22:18 +0900

    wip

--- README.md
+++ README.md
@@ -6,9 +6,25 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `npx cdk deploy` deploy this stack to your default AWS account/region
+- `npx cdk diff` compare deployed stack with current state
+- `npx cdk synth` emits the synthesized CloudFormation template
+
+## 手動で触らないこと
+
+CIDR ブロックや Private IP アドレスを直接指定しないこと。
+
+## ポイント
+
+### EC2 における SSM の有効化
+
+- CDKのEC2記述
+  - 443 インバウンドポートの開放
+  - ssmSessionPermissions: true
+- ローカルの準備
+  - SessionManagerPlugin のインストール
+    - https://docs.aws.amazon.com/systems-manager/latest/userguide/install-plugin-debian-and-ubuntu.html
+
--- lib/aws-stack.ts
+++ lib/aws-stack.ts
@@ -5,17 +5,66 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 export class AwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    // const vpc = new ec2.Vpc(this, "SampleVpca", {
-    //   vpcName: "sample-vpc-changed",
-    //   ipAddresses: ec2.IpAddresses.cidr("172.16.0.0/16"),
-    //   subnetConfiguration: [],
-    //   natGateways: 0
-    // });
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    ///////////////
+    // VPC
+    ///////////////
+    const vpc = new ec2.Vpc(this, "DefaultVPC", {
+      vpcName: "default-vpc",
+      ipAddresses: ec2.IpAddresses.cidr("172.16.0.0/16"),
+      maxAzs: 1, // 指定しないとデフォルト値が3なので、最大3つ作られてしまう。
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "PublicMain",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "PrivateMain",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+      natGateways: 0,
+    });
+
+    ///////////////
+    // Security Group
+    ///////////////
+    const sshSecurityGroup = new ec2.SecurityGroup(this, "SSHSecurityGroup", {
+      vpc: vpc,
+      description: "Security Group for SSH",
+      allowAllOutbound: true,
+    });
+    sshSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22));
+
+    const ssmSecurityGroup = new ec2.SecurityGroup(this, "SSMSecurityGroup", {
+      vpc: vpc,
+      description: "Security Group for SSM",
+      allowAllOutbound: true,
+    });
+    ssmSecurityGroup.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(443));
+
+    ///////////////
+    // EC2 踏み台
+    ///////////////
+    const bastion = new ec2.Instance(this, "Bastion", {
+      vpc: vpc,
+      instanceName: "bastion",
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T4G,
+        ec2.InstanceSize.NANO
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2023,
+        cpuType: ec2.AmazonLinuxCpuType.ARM_64,
+      }),
+      vpcSubnets: {
+        subnetGroupName: "PublicMain",
+      },
+      securityGroup: sshSecurityGroup,
+      ssmSessionPermissions: true, // SSMによるアクセスを許可
+    });
+    bastion.addSecurityGroup(ssmSecurityGroup);
   }
 }
